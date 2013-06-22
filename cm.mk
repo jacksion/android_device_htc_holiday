1cda752b2af6a15ebb5033edaeb0beeebece7ea8
Dorian Snyder <dastin1015@gmail.com>
2013-06-21 23:50:57 -0700

    holiday: some inital cleanup

--- cm.mk
+++ cm.mk
@@ -3,12 +3,8 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Preload bootanimation
-TARGET_BOOTANIMATION_NAME := 540
-
 # Inherit device configuration
 $(call inherit-product, device/htc/holiday/device_holiday.mk)
-
 
 # Device naming
 PRODUCT_DEVICE := holiday
@@ -16,6 +12,10 @@ PRODUCT_NAME := cm_holiday
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := Vivid 4G
 PRODUCT_MANUFACTURER := HTC
+
+# Bootanimation
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_holiday BUILD_FINGERPRINT=cingular_us/htc_holiday/holiday:4.0.3/IML74K/302737.56:user/release-keys PRIVATE_BUILD_DESC="3.26.502.56 CL302737 release-keys"
