29840e0804fd19b9e1e249feb2456163f898de74
Matt Filetto <matt.filetto@gmail.com>
2013-10-21 00:37:26 -0700

    BoardConfig: remove no-op audio flag from testing
    Change-Id: I2b7d87bf548c1289fc740aa79e50ca69a3983ca6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,9 +45,6 @@ BOARD_PROVIDES_LIBRIL := true
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := holiday
 
-# Audio
-BOARD_HAVE_AUDIENCE_ES325_2MIC := true
-
 # USB
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
 
