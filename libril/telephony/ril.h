ee77c075b58c37d7226d5527b497c8d3a688ff81
Matt Filetto <matt.filetto@gmail.com>
2013-10-20 18:38:24 -0700

    even more header reverts
    Change-Id: I3126aeddcdef70dd5c6f9feb22cb033c8fc9a0ec

--- libril/telephony/ril.h
+++ libril/telephony/ril.h
@@ -672,26 +672,9 @@ typedef struct {
 } RIL_EVDO_SignalStrength;
 
 typedef struct {
-    int signalStrength;  /* Valid values are (0-31, 99) as defined in TS 27.007 8.5 */
-    int rsrp;            /* The current Reference Signal Receive Power in dBm multipled by -1.
-                          * Range: 44 to 140 dBm
-                          * INT_MAX: 0x7FFFFFFF denotes invalid value.
-                          * Reference: 3GPP TS 36.133 9.1.4 */
-    int rsrq;            /* The current Reference Signal Receive Quality in dB multiplied by -1.
-                          * Range: 20 to 3 dB.
-                          * INT_MAX: 0x7FFFFFFF denotes invalid value.
-                          * Reference: 3GPP TS 36.133 9.1.7 */
-    int rssnr;           /* The current reference signal signal-to-noise ratio in 0.1 dB units.
-                          * Range: -200 to +300 (-200 = -20.0 dB, +300 = 30dB).
-                          * INT_MAX : 0x7FFFFFFF denotes invalid value.
-                          * Reference: 3GPP TS 36.101 8.1.1 */
-    int cqi;             /* The current Channel Quality Indicator.
-                          * Range: 0 to 15.
-                          * INT_MAX : 0x7FFFFFFF denotes invalid value.
-                          * Reference: 3GPP TS 36.101 9.2, 9.3, A.4 */
     int dbm;
     int ecno;
-} RIL_LTE_SignalStrength;
+} RIL_ATT_SignalStrength;
 
 typedef struct {
     int signalStrength;  /* Valid values are (0-31, 99) as defined in TS 27.007 8.5 */
@@ -717,7 +700,7 @@ typedef struct {
                           * INT_MAX : 0x7FFFFFFF denotes invalid value.
                           * Reference: 3GPP 36.321 section 6.1.3.5
                           * also: http://www.cellular-planningoptimization.com/2010/02/timing-advance-with-calculation.html */
-} RIL_LTE_SignalStrength_v8;
+} RIL_LTE_SignalStrength;
 
 /* Deprecated, use RIL_SignalStrength_v6 */
 typedef struct {
