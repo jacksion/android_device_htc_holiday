f8eee0a27f6323998a86a9d40dde6cb9e216a59d
Matt Filetto <matt.filetto@gmail.com>
2013-10-23 23:05:33 -0700

    libril: latest ruby ril (EV)
    Change-Id: I60ecde4c8fa27a63ef4eb061748d3f23f199a94a

--- libril/Android.mk
+++ libril/Android.mk
@@ -11,12 +11,16 @@ LOCAL_SRC_FILES:= \
     ril_event.cpp
 
 LOCAL_SHARED_LIBRARIES := \
+    liblog \
     libutils \
     libbinder \
     libcutils \
     libhardware_legacy
 
 LOCAL_CFLAGS :=
+ifdef BOARD_USE_NEW_LIBRIL_HTC
+    LOCAL_CFLAGS += -DNEW_LIBRIL_HTC
+endif
 
 LOCAL_MODULE:= libril
 
